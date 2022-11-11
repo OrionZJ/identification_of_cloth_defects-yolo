2eb0b14050dc5e399a73a287f848f57979296c8d
OrionZJ <hcmcj720@163.com>
2022-11-11 21:16:35 +0800

    增加图像切割层

--- detect.py
+++ detect.py
@@ -120,6 +120,46 @@ def run(
                 im = im[None]  # expand for batch dim
 
         # Inference
+        mulpicplus = "3"     #1 for normal,2 for 4pic plus,3 for 9pic plus and so on
+        assert(int(mulpicplus)>=1)
+        if mulpicplus == "1":
+            pred = model(im,
+                          augment=augment,
+                          visualize=increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False)[0]
+
+        else:
+            xsz = im.shape[2]
+            ysz = im.shape[3]
+            mulpicplus = int(mulpicplus)
+            x_smalloccur = int(xsz / mulpicplus * 1.2)
+            y_smalloccur = int(ysz / mulpicplus * 1.2)
+            for i in range(mulpicplus):
+                x_startpoint = int(i * (xsz / mulpicplus))
+                for j in range(mulpicplus):
+                    y_startpoint = int(j * (ysz / mulpicplus))
+                    x_real = min(x_startpoint + x_smalloccur, xsz)
+                    y_real = min(y_startpoint + y_smalloccur, ysz)
+                    if (x_real - x_startpoint) % 64 != 0:
+                        x_real = x_real - (x_real-x_startpoint) % 64
+                    if (y_real - y_startpoint) % 64 != 0:
+                        y_real = y_real - (y_real - y_startpoint) % 64
+                    dicsrc = im[:, :, x_startpoint:x_real,
+                                                    y_startpoint:y_real]
+                    pred_temp = model(dicsrc,
+                                    augment=augment,
+                                    visualize=increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False)[0]
+                    pred_temp[..., 0] = pred_temp[..., 0] + y_startpoint
+                    pred_temp[..., 1] = pred_temp[..., 1] + x_startpoint
+                    if i==0 and j == 0:
+                        pred = pred_temp
+                    else:
+                        pred = torch.cat([pred, pred_temp], dim=1)
+
+
+        # Apply NMS
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+
+
         with dt[1]:
             visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
             pred = model(im, augment=augment, visualize=visualize)
